3e3b61e824332e3fc0c9ceafc7b29878ef94ad48
Siripat Srisuwannaphat <47081187+iristap@users.noreply.github.com>
2025-10-30 23:27:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from src.mlProject.config import configuration
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
